e382ef42d74844c3df2cf994fe3557f75d289f2a
Adam Sornoso <asornoso@yahoo.com>
2016-04-24 14:25:14 -0700

    tests

--- src/test/java/edu/csula/datascience/acquisition/CollectorTest.java
+++ src/test/java/edu/csula/datascience/acquisition/CollectorTest.java
@@ -42,60 +42,60 @@ public class CollectorTest {
             Assert.assertEquals(list.get(i).getContent(), expectedList.get(i).getContent());
         }
     }
-    
-    @Test
-    public void mungeeSingleton() throws Exception {
-
-        List<MockData2> list = Lists.newArrayList(
-        
-        		new MockData2("0", null, "adams", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-        		new MockData2("1", "1111", null, "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-        		new MockData2("2", "2222", "davidp", null, 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-        		new MockData2("3", "3333", "adams", "this is a tweet again", -1,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-        		new MockData2("4", "4444", "davida", "this is a tweet", 0, "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-        		new MockData2("5", "5555", "davida", "this is a tweet", 0, null, 0, false, false),
-       			new MockData2("6", "6666", "davidp", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", -1, false, false),
-				new MockData2("7", "7777", "adams", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-				new MockData2("8", "4444", "davida", "this is a tweet", 0,"Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-				new MockData2("9", "9999", "adams", "this is a tweet", 0, "Fri Apr 22 17:08:45 PDT 2016", 0, false, false)
-
-        );
-        
-        List<MockData2> cleaned = new ArrayList<MockData2>();
-        
-        for(int i = 0; i < list.size(); i++)
-        {
-        /*	if(collector.mungeeSingleton(list.get(i)))
-        	{
-        		cleaned.add(list.get(i));
-        	}
-        	*/
-        }
-        
-        
-        List<MockData2> expectedList = Lists.newArrayList(
-        	new MockData2("8", "4444", "davida", "this is a tweet", 0, "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
-        	new MockData2("9", "9999", "adams", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false)
-           
-        );
-
-        Assert.assertEquals(list.size(), 2);
-
-        for (int i = 0; i < 2; i ++) {
-            Assert.assertEquals(list.get(i).getId(), expectedList.get(i).getId());
-            Assert.assertEquals(list.get(i).getTweetId(), expectedList.get(i).getTweetId());
-            Assert.assertEquals(list.get(i).getDate(), expectedList.get(i).getDate());
-            Assert.assertEquals(list.get(i).getFav(), expectedList.get(i).getFav());
-            Assert.assertEquals(list.get(i).getRetweet(), expectedList.get(i).getRetweet());
-            Assert.assertEquals(list.get(i).isRetweeted(), expectedList.get(i).isRetweeted());
-            Assert.assertEquals(list.get(i).getText(), expectedList.get(i).getText());
-            Assert.assertEquals(list.get(i).getUsername(), expectedList.get(i).getUsername());
-            Assert.assertEquals(list.get(i).isSensitive(), expectedList.get(i).isSensitive());
-
-            
-          //  Assert.assertEquals(list.get(i).getContent(), expectedList.get(i).getContent());
-        }
-    }
+//    
+//    @Test
+//    public void mungeeSingleton() throws Exception {
+//
+//        List<MockData2> list = Lists.newArrayList(
+//        
+//        		new MockData2("0", null, "adams", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//        		new MockData2("1", "1111", null, "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//        		new MockData2("2", "2222", "davidp", null, 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//        		new MockData2("3", "3333", "adams", "this is a tweet again", -1,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//        		new MockData2("4", "4444", "davida", "this is a tweet", 0, "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//        		new MockData2("5", "5555", "davida", "this is a tweet", 0, null, 0, false, false),
+//       			new MockData2("6", "6666", "davidp", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", -1, false, false),
+//				new MockData2("7", "7777", "adams", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//				new MockData2("8", "4444", "davida", "this is a tweet", 0,"Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//				new MockData2("9", "9999", "adams", "this is a tweet", 0, "Fri Apr 22 17:08:45 PDT 2016", 0, false, false)
+//
+//        );
+//        
+//        List<MockData2> cleaned = new ArrayList<MockData2>();
+//        
+//        for(int i = 0; i < list.size(); i++)
+//        {
+//        /*	if(collector.mungeeSingleton(list.get(i)))
+//        	{
+//        		cleaned.add(list.get(i));
+//        	}
+//        	*/
+//        }
+//        
+//        
+//        List<MockData2> expectedList = Lists.newArrayList(
+//        	new MockData2("8", "4444", "davida", "this is a tweet", 0, "Fri Apr 22 17:08:45 PDT 2016", 0, false, false),
+//        	new MockData2("9", "9999", "adams", "this is a tweet", 0,  "Fri Apr 22 17:08:45 PDT 2016", 0, false, false)
+//           
+//        );
+//
+//        Assert.assertEquals(list.size(), 2);
+//
+//        for (int i = 0; i < 2; i ++) {
+//            Assert.assertEquals(list.get(i).getId(), expectedList.get(i).getId());
+//            Assert.assertEquals(list.get(i).getTweetId(), expectedList.get(i).getTweetId());
+//            Assert.assertEquals(list.get(i).getDate(), expectedList.get(i).getDate());
+//            Assert.assertEquals(list.get(i).getFav(), expectedList.get(i).getFav());
+//            Assert.assertEquals(list.get(i).getRetweet(), expectedList.get(i).getRetweet());
+//            Assert.assertEquals(list.get(i).isRetweeted(), expectedList.get(i).isRetweeted());
+//            Assert.assertEquals(list.get(i).getText(), expectedList.get(i).getText());
+//            Assert.assertEquals(list.get(i).getUsername(), expectedList.get(i).getUsername());
+//            Assert.assertEquals(list.get(i).isSensitive(), expectedList.get(i).isSensitive());
+//
+//            
+//          //  Assert.assertEquals(list.get(i).getContent(), expectedList.get(i).getContent());
+//        }
+//    }
     
     
     
--- src/test/java/edu/csula/datascience/acquisition/MockCollector2.java
+++ src/test/java/edu/csula/datascience/acquisition/MockCollector2.java
@@ -1,23 +1,59 @@
 package edu.csula.datascience.acquisition;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.Lists;
+
+import twitter4j.Status;
 
 /**
  * A mock implementation of collector for testing
  */
-public class MockCollector2 implements Collector<SimpleModel, MockData> {
+public class MockCollector2 implements Collector<MockData2, MockData2> {
+	
     @Override
-    public Collection<SimpleModel> mungee(Collection<MockData> src) {
+    public Collection<MockData2> mungee(Collection<MockData2> src) {
         // in your example, you might need to check src.hasNext() first
-        return src
-            .stream()
-            .filter(data -> data.getContent() != null)
-            .map(SimpleModel::build)
-            .collect(Collectors.toList());
+//        return src
+//            .stream()
+//            .filter(data -> data.getContent() != null)
+//            .map(SimpleModel::build)
+//            .collect(Collectors.toList());
+//        
+//        
+    	
+    	List<MockData2> list = Lists.newArrayList();
+    	List<MockData2> cleaned = Lists.newArrayList();
+    	if(src !=null && !src.isEmpty())
+    	{
+    		list.addAll(src);
+    		
+    		
+	    		
+	    		for (int i = 0; i < list.size(); i++)
+		    	{
+		        	if (   list.get(i).getId() != null && list.get(i).getTweetId() != null 
+		        			&& list.get(i).getUsername() != null && list.get(i).getFav() >= 0 && list.get(i).getText() != null
+		        			&& list.get(i).getRetweet() >= 0 && list.get(i).getDate() != null )
+		    		{
+		    			cleaned.add(list.get(i)); // add that tweet from the document
+		    		}
+		        	System.out.println("INDEX: "+i);
+		        	
+		    	}
+	    	
+    	}
+        return cleaned;
+        
     }
 
-    @Override
-    public void save(Collection<SimpleModel> data) {
-    }
+	@Override
+	public void save(Collection<MockData2> data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
